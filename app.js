6d8acc130d436ddf1c42bc2617dec2cfa34a911f
Matias Cuervo <matias.a.acuervo@gmail.com>
2021-04-13 02:25:30 -0300

    xd

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const btn= document.getElementById('start-btn')
 const text= document.getElementById('text')
 const descargar= document.getElementById('descarga-btn')
+
+const greetings='Hola anto bomboncito, entregame el rosquete querida dale no te hagas la dificl'
+
 if(text.textContent=""){
     descargar.classList.toggle('is-disable')
 }
@@ -20,8 +23,23 @@ recognition.onresult=(event)=>{
 
     const transcript=event.results[current][0].transcript
     text.value=transcript
+    readingThisShit(transcript)
 }
 
 btn.addEventListener('click',()=>{
     recognition.start()
 })
+
+const readingThisShit=(message)=>{
+    const speech= new SpeechSynthesisUtterance()
+    speech.text='sacate la papa de la boca'
+    if(message.includes('hola soy anto')){
+        speech.text=greetings
+    }
+    
+    speech.volume=1
+    speech.rate=1
+    speech.pitch=1
+
+    window.speechSynthesis.speak(speech)
+}
